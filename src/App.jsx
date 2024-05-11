59a2de08db886a8ada200337ba7eb4c5b0c958f0
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2024-05-11 13:36:41 +0545

    login and registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NaRoutes, Rout } from "react-router-dom";
+import React from "react";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
 
 function App() {
   return (
     <>
-      <div className="max-w-6xl mx-auto text-4xl">
+      <div className="bg-slate-200 text-4xl min-h-screen">
         <nav className="rounded bg-indigo-900 text-white px-2 py-2.5 sm:px-4">
           <div className="container mx-auto flex flex-wrap items-center justify-between">
             <a href="https://laraveller.com/" className="flex items-center">
-              Laraveller
+              Personal Signature
             </a>
             <div className="hidden w-full md:block md:w-auto">
               <ul className="mt-4 flex flex-col rounded-lg p-4 md:mt-0 md:flex-row space-x-8 md:text-sm md:font-medium">
@@ -42,6 +46,13 @@ function App() {
             </div>
           </div>
         </nav>
+        <div className="max-w-7xl max-auto mt-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
       </div>
     </>
   );
