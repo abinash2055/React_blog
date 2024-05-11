bf885f4daea9374271407ff7a763452cf6558d25
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2024-05-11 12:44:04 +0545

    Created Login register nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
+import { Link, NavLink } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <div className="max-w-6xl m-auto text-4xl">WELCOME </div>
+      <div className="max-w-6xl mx-auto text-4xl">
+        <nav className="rounded bg-indigo-900 text-white px-2 py-2.5 sm:px-4">
+          <div className="container mx-auto flex flex-wrap items-center justify-between">
+            <a href="https://laraveller.com/" className="flex items-center">
+              Laraveller
+            </a>
+            <div className="hidden w-full md:block md:w-auto">
+              <ul className="mt-4 flex flex-col rounded-lg p-4 md:mt-0 md:flex-row space-x-8 md:text-sm md:font-medium">
+                <li>
+                  <Link
+                    to="/"
+                    class="block rounded py-2 pr-4 pl-3 text-white"
+                    aria-current="page"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/login"
+                    class="block rounded py-2 pr-4 pl-3 text-white"
+                    aria-current="page"
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/register"
+                    class="block rounded py-2 pr-4 pl-3 text-white"
+                    aria-current="page"
+                  >
+                    Register
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
     </>
   );
 }
